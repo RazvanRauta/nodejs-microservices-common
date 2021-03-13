a45583c089203bad4fb67bd4b2b87cf1bcc44182
Razvan <razvan.rauta@gmail.com>
2021-03-13 17:28:50 +0300

    Console log unhandled error

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -2,16 +2,24 @@ import { Request, Response, NextFunction } from 'express'
 import { CustomError } from '../errors/custom-error'
 
 export const errorHandler = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  if (err instanceof CustomError) {
-    return res.status(err.statusCode).send({ errors: err.serializeErrors() })
-  }
+    if (err instanceof CustomError) {
+        return res
+            .status(err.statusCode)
+            .send({ errors: err.serializeErrors() })
+    }
 
-  res.status(400).send({
-    errors: [{ message: 'Something went wrong' }],
-  })
+    console.error(
+        '\n<----- TICKETING ERROR ----->\n\n',
+        err,
+        '\n\n<----- TICKETING ERROR ----->\n'
+    )
+
+    res.status(400).send({
+        errors: [{ message: 'Something went wrong' }],
+    })
 }
