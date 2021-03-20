447efdcba6aed3a713e700b6ecb06ae2ddf91ba0
Razvan <razvan.rauta@gmail.com>
2021-03-20 19:32:38 +0300

    Updates

---
+++ src/events/payment-created-event.ts
@@ -0,0 +1,16 @@
+/**
+ * @ @author: Razvan Rauta
+ * @ Date: Mar 13 2021
+ * @ Time: 21:12
+ */
+
+import { Subjects } from './subjects'
+
+export interface PaymentCreatedEvent {
+    subject: Subjects.PaymentCreated
+    data: {
+        id: string
+        orderId: string
+        stripeId: string
+    }
+}
